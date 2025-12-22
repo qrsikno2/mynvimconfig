0952790cf4188c3171e7cd4a04210364443dcfc3
qrsikno2 <qrsikno@163.com>
2025-12-22 20:46:59 +0800

    added vscode configuration

--- lua/vsvim/init.lua
+++ lua/vsvim/init.lua
@@ -1,3 +1,4 @@
+local vscode = require("vscode")
 local opts = {
 	noremap = true, -- non-recursive
 	silent = true, -- do not show message
@@ -33,3 +34,49 @@ vim.keymap.set("v", "J", "<C-d>", opts)
 vim.keymap.set("v", "K", "<C-u>", opts)
 
 vim.keymap.set("n", "<space><space>", "<C-w><C-w>", opts)
+
+vim.keymap.set("n", "<space>e", function()
+	-- 在VSCode中，显示当前行的诊断信息，通常使用命令`editor.action.showHover`，但它会显示所有信息，包括类型信息等。
+	-- 但是，VSCode没有单独显示诊断的命令。我们可以使用以下方式：
+	-- 先将光标移动到诊断信息上，然后触发悬停。但这样不够直接。
+	-- 另一种方式是打开问题面板，但这不是悬浮窗口。
+	-- 所以，这里我们可能需要调整：在VSCode中，通常按Ctrl+`（反引号）会显示当前行的错误，但这是VSCode的默认行为。
+	-- 由于VSCode的LSP集成方式不同，我们可能需要使用VSCode的命令。
+	vscode.action("editor.action.showHover")
+end, { noremap = true, silent = true })
+
+-- 跳转上一个/下一个诊断
+vim.keymap.set("n", "[d", function()
+	vscode.action("editor.action.marker.prevInFiles")
+end, { noremap = true, silent = true })
+vim.keymap.set("n", "]d", function()
+	vscode.action("editor.action.marker.nextInFiles")
+end, { noremap = true, silent = true })
+
+-- 打开问题面板（相当于setloclist）
+vim.keymap.set("n", "<space>q", function()
+	vscode.action("workbench.panel.markers.view.focus")
+end, { noremap = true, silent = true })
+
+-- LSP通用命令
+vim.keymap.set("n", "gD", function()
+	vscode.action("editor.action.revealDefinition")
+end, { noremap = true, silent = true })
+
+vim.keymap.set("n", "gi", function()
+	vscode.action("editor.action.goToImplementation")
+end, { noremap = true, silent = true })
+
+vim.keymap.set("n", "gs", function()
+	vscode.action("editor.action.triggerParameterHints")
+end, { noremap = true, silent = true })
+
+-- 类型定义
+vim.keymap.set("n", "<space>D", function()
+	vscode.action("editor.action.goToTypeDefinition")
+end, { noremap = true, silent = true })
+
+-- for gr gotoReferences
+vim.keymap.set("n", "gr", function()
+	vscode.action("editor.action.goToReferences")
+end, { noremap = true, silent = true })
